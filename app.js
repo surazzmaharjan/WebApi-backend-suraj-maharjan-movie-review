17c0f08c8209ea60e211e8add0c06621edb22274
Suraj Maharjan <surajmzn75@gmail.com>
2020-01-10 11:54:04 +0545

    setup app.js file

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app= express();
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const dotenv = require('dotenv').config();
+
+require('./database/mongoose');
+const userRouter = require('./routers/user');
+
+app.use(bodyParser.urlencoded({ extended:                                                                                                                                                                                                                                                                                                                                                                                                                                                                   false }));
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.use(userRouter);
+
+
+//server port 
+app.listen(process.env.PORT, () => {
+    console.log(`App is running at localhost:${process.env.PORT}`);
+});
