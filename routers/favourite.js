4deb931cfda79790689b0c2de73cd785fd07250a
Suraj Maharjan <surajmzn75@gmail.com>
2020-01-19 12:58:39 +0545

    added retrieve and delete function

--- routers/favourite.js
+++ routers/favourite.js
@@ -22,4 +22,25 @@ router.post('/favourite',auth.verifyUser, (req, res) => {
     });
 });
 
+router.get('/favourite',auth.verifyUser, (req, res, next) => {
+    Favourite.find().then(function(favourite){
+        res.json(favourite);
+
+    }).catch(function(e){
+
+            res.json(e)
+
+    });
+});
+
+
+router.delete('/favourite/:id',auth.verifyUser,(req,res)=>{
+
+    Favourite.findByIdAndDelete(req.params.id).then(function(){
+                res.status(200).json({code:200,message:"Succesfully deleted from favourite"});
+            }).catch(function(e){
+                res.send(e)
+            });
+        });
+
 module.exports=router;
